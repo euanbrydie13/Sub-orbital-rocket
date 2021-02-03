5ddf5730809c3dd05454163461e894c8f972ecb8
euanbrydie13 <60703260+euanbrydie13@users.noreply.github.com>
2021-02-03 16:40:45 +0000

    Update propulsion_model.m

--- Group-N/Sub_Orbital_Simulation/propulsion_model.m
+++ Group-N/Sub_Orbital_Simulation/propulsion_model.m
@@ -4,4 +4,5 @@ function [FT, mp] = propulsion_model(throttle,vehicle,press)
 FT = throttle.*(vehicle.tvac + vehicle.aexit.*(1e5 - press));       % Force of Thrust [N]
 mp = throttle.*(FT./(9.81.*vehicle.isp));                           % Mass flow [kgs-1]
 
+%gfhh
 end
