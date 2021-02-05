66e2628e5391a160ec9141effdb226f7271111f5
PatrickO99 <72758439+PatrickO99@users.noreply.github.com>
2021-02-05 14:39:47 +0000

    Update propulsion_model.m

--- Group-N/Sub_Orbital_Simulation/propulsion_model.m
+++ Group-N/Sub_Orbital_Simulation/propulsion_model.m
@@ -1,7 +1,6 @@
 function [FT, mp] = propulsion_model(throttle,vehicle,press)
 
-
-FT = throttle.*(vehicle.tvac + vehicle.aexit.*(1e5 - press));       % Force of Thrust [N]
-mp = throttle.*(FT./(9.81.*vehicle.isp));                           % Mass flow [kgs-1]
-
+mp = throttle.*(FT./(9.81.*vehicle.isp));                              % Mass flow [kgs-1] - Could be fluid density at nozzle x exhaust velocity x nozzle exit area
+FT = throttle.*(vehicle.tvac*mp + vehicle.aexit.*(1e5 - press));       % Force of Thrust [N] - Changed by Paddy
+                        
 end
