5b0495291bad32ed1ac2385ea50d93946bd0e162
euanbrydie13 <60703260+euanbrydie13@users.noreply.github.com>
2021-02-06 18:41:45 +0000

    chi and fpa

--- Group-N/Sub_Orbital_Simulation/propulsion_model.m
+++ Group-N/Sub_Orbital_Simulation/propulsion_model.m
@@ -1,8 +1,8 @@
 function [FT, mp] = propulsion_model(throttle,vehicle,press)
 
+FT = throttle.*(vehicle.tvac + vehicle.aexit.*(1e5 - press));          % Force of Thrust [N]
+mp = throttle.*(FT./(9.81.*vehicle.isp));                             % Mass flow [kgs-1]
 
-FT = throttle.*(vehicle.tvac + vehicle.aexit.*(1e5 - press));       % Force of Thrust [N]
-mp = throttle.*(FT./(9.81.*vehicle.isp));                           % Mass flow [kgs-1]
-
-%gfhh
+% mp = vehicle.aexit*vehicle.exitV*vehicle.fluidD;                       %throttle.*(FT./(9.81.*vehicle.isp));                           % Mass flow [kgs-1]
+% FT = throttle.*(vehicle.tvac*mp + vehicle.aexit.*(1e5 - press));       % Force of Thrust [N]
 end
